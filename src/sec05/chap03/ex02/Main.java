d03375dd44a389e1ae7dea964f8c87d42d1e06d8
noah <jeonghogim318@gmail.com>
2025-01-05 14:00:00 +0900

    클래스(정적) 필드와 메소드

---
+++ src/sec05/chap03/ex02/Main.java
@@ -0,0 +1,8 @@
+package sec05.chap03.ex02;
+
+public class Main {
+    YalcoChicken store1 = new YalcoChicken("판교");
+    YalcoChicken store2 = new YalcoChicken("강남");
+    YalcoChicken store3 = new YalcoChicken("제주");
+
+}
