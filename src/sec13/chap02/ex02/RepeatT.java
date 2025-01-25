48cecdb6703bcefe0c1dcdcbfe5ab2b1b2b6e35b
noah <jeonghogim318@gmail.com>
2025-01-25 12:00:00 +0900

    표준 & 메타 어노테이션

---
+++ src/sec13/chap02/ex02/RepeatT.java
@@ -0,0 +1,12 @@
+package sec13.chap02.ex02;
+
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+
+@Repeatable(Repeats.class)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RepeatT {
+    //  💡 다음 강에서 배움
+    int a();
+    int b();
+}
---
+++ src/sec13/chap02/ex02/RetSource.java
@@ -0,0 +1,6 @@
+package sec13.chap02.ex02;
+
+import java.lang.annotation.Retention;
+
+@Retention(RetentionPolicy.SOURCE)
+public @interface RetSource { }
